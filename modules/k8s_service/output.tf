568f12ea86fd08808df4d94d421a5ead7489a0d4
Luiz Filipe <luiz.filipe@neomed.tech>
2023-06-20 01:33:31 -0300

    adicionando output para service

--- modules/k8s_service/output.tf
+++ modules/k8s_service/output.tf
@@ -5,3 +5,7 @@ output "service_name" {
 output "service_port" {
   value = var.service_port
 }
+
+output "service_endpoint" {
+  value = kubernetes_service.service.status.0.load_balancer.0.ingress.0.ip
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "netflix_endpoint" {
+  value = module.netflix-service.service_endpoint
+}
